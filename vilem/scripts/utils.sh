5a835866a7880d68a19e3d3658f77481cde5b65a
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-07-30 22:05:00 +0200

    cluster is down so just running stuff locally

--- vilem/scripts/utils.sh
+++ vilem/scripts/utils.sh
@@ -25,11 +25,21 @@ function get_config() {
 function sbatch_gpu() {
     JOB_NAME=$1;
     JOB_WRAP=$2;
+    mkdir -p logs
+
     sbatch \
         -J $JOB_NAME --output=logs/%x.out --error=logs/%x.err \
         --gpus=1 --gres=gpumem:22g \
         --ntasks-per-node=1 \
         --cpus-per-task=6 \
-        --mem-per-cpu=8G --time=0-8 \
+        --mem-per-cpu=8G --time=1-0 \
         --wrap="$JOB_WRAP";
+}
+
+function local_gpu() {
+    JOB_NAME=$1;
+    JOB_WRAP=$2;
+    mkdir -p logs
+
+    $JOB_WRAP 1> logs/${JOB_NAME}.out 2> logs/${JOB_NAME}.err
 }
